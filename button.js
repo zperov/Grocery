37a3500113a48d783bdb8e4978f5977c3784a034
zperov <eperov2@gmail.com>
2025-10-05 11:58:08 -0400

    Update button.js

--- button.js
+++ button.js
@@ -2,7 +2,7 @@ const saleButton = document.getElementById('saleButton');
 const saleItem = document.getElementById('saleItem');
 
 saleButton.addEventListener('click', () => {
-  if (saleItem.style.display === 'none') {
+  if (saleItem.style.display === 'none' || saleItem.style.display === '') {
     saleItem.style.display = 'block';
   } else {
     saleItem.style.display = 'none';
