4d53daff626e591c74fea768c3625279f547583d
zperov <eperov2@gmail.com>
2025-10-12 16:07:23 -0400

    Update contactForm.php

--- contactForm.php
+++ contactForm.php
@@ -1,16 +1,15 @@
 <?php
       if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (!empty($_POST['nm']) && !empty($_POST['ph']) && !empty($_POST['Input Here'])) {
         $nm = htmlspecialchars($_POST['name']);
         $ph = htmlspecialchars($_POST['phone']);
         $em = htmlspecialchars($_POST['email']);
         $mes = htmlspecialchars($_POST['message']);
         $ordNum = htmlspecialchars($_POST['orNum']);
-        
+        if (empty($nm) || empty($ph) || empty($mes)) {
+        die("Name, phone, and message are required.")else {
         echo "<h1>Information Captured</h1>";
         echo "<p>Thank you for your submission, $nm.</p>";
-        
-    } else {
+      } else {
         echo "<h1>Error</h1>";
         echo "<p>Please go back and fill in all the required fields.</p>";
     }
