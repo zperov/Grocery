6c2904f0e47ada191e16949cd7cb4ca740127442
zperov <eperov2@gmail.com>
2025-10-12 22:50:50 -0400

    Update contactForm.php

--- contactForm.php
+++ contactForm.php
@@ -1,5 +1,5 @@
 <?php
-      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $nm = htmlspecialchars($_POST['nm']);
         $ph = htmlspecialchars($_POST['ph']);
         $em = htmlspecialchars($_POST['em']);
@@ -10,6 +10,5 @@
         else {
         echo "<h1>Information Captured</h1>";
         echo "<p>Thank you for your submission, $nm.</p>";
-      } else    
-} 
+      } 
 ?>
